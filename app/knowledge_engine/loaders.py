103fcfcc96a57e5570f9e3436de4f17bf9ad87af
Tanmay Yadav <tanmayyadav300@gmail.com>
2026-02-15 17:48:41 +0530

    Adding simple pdf and docx loader method first commit

--- app/knowledge_engine/loaders.py
+++ app/knowledge_engine/loaders.py
@@ -1,1 +1,19 @@
-# PDF/DOCX loaders
+# PDF/DOCX loaders 
+import os
+from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def get_loader(file_path):
+    ext = os.path.splitext(file_path)[1].lower()
+
+    if ext == ".pdf":
+        return PyPDFLoader(file_path)
+    elif ext == ".docx":
+        return Docx2txtLoader(file_path)
+    else:
+        raise ValueError("Unsupported file type")
+    
+loader = get_loader("E:\\vinayak\\RFPForge\\data\\knowledge_docs\\RFP_MAPP10022014.pdf")
+documents = loader.load()
+print(f"Loaded {len(documents)} documents.")
+print(documents[10].page_content[:500])  # Print the first 500 characters of the first document
