fd19ded723b80d76ab6a155fee327731b5fe8b21
Tanmay Yadav <tanmayyadav300@gmail.com>
2026-02-20 14:37:33 +0530

    Retrieval.py first commit

--- app/knowledge_engine/retrieval.py
+++ app/knowledge_engine/retrieval.py
@@ -1,1 +1,9 @@
-# top-k + reranking
+# top-k + reranking 
+query ="Here will be the users query"
+search_results=vectorestore.similarity_search(query,k=2)
+print(f"\nTop 2 most relevant chunks for the query:'{query}'\n")
+for i, result in enumerate(search_results,1):
+    print(f"Result {i}:")
+    print(f"Source: {result.metadata.get('source','unknown')}")
+    print(f"Content: {result.page_content}")
+    print()
